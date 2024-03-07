3ae665c8d5147dd0e03657f9d28fd1ed1f2283c9
Roblox_Thot <67937010+Roblox-Thot@users.noreply.github.com>
2024-03-07 14:40:46 -0500

    иуые кщидщч рфсл

--- FolderUploader.py
+++ FolderUploader.py
@@ -1,4 +1,5 @@
 # TODO: make/finish this
+# TODO: find the max keys per sec lmao - its 6 ig
 
 from DecalUploader import DecalClass, Functions
 from rblxopencloud import exceptions
@@ -6,27 +7,58 @@ import random, os, threading
 from time import sleep as sleepy
 
 class ThreadShit:
-    def start(files: list,barrier):
+    def upload(creator:DecalClass, filename:str, title:str, discription:str, outfile, barrier:threading.Barrier):
         barrier.wait()
-        print(len(files),__import__('datetime').datetime.now())
+        print(filename)
+        with open(f'decals/{filename}', "rb") as file:
+            while True: # keep uploading till one works :)
+                try:
+                    asset = creator.upload(file, title, discription)
+                    break
+                except Exception as e:
+                    if e == exceptions.RateLimited:
+                        sleepy(1)
+                        print('rate limit')
 
-class Functions:
+        img_id = Functions.get_image_id(asset.id)
+        outfile.write(f'{filename},{asset.id},{img_id}\n')
+
+    def start(files: list, ROBLOSECURITY:str, outfile):
+        creator = DecalClass(ROBLOSECURITY)
+        barrierh = threading.Barrier(len(files)+1)
+        threads = []
+        for i in files:
+            thread = threading.Thread(target=ThreadShit.upload, args=(creator,i,"RT","Tools",outfile, barrierh,))
+            thread.start()
+            threads.append(thread)
+        
+        barrierh.wait()
+
+        for thread in threads:
+            thread.join()
+
+class FolderFunctions:
     def split_list(input_list, max_length=60):
         """
         Split a list into sublists with a maximum length.
         """
-        return [input_list[i:i+max_length] for i in range(0, len(input_list), max_length)]
+        return [input_list[i:i+max_length] for i in range(0, len(input_list), max_length)][:6]
 
 if __name__ == '__main__':
+    ROBLOSECURITY = input("Cookie: ")
     files = os.listdir('decals')
-    split_files = Functions.split_list(files)
-    barrier = threading.Barrier(len(split_files) + 1)
+    split_files = FolderFunctions.split_list(files)
+    outfile = open("Outa.csv",'w')
+    outfile.write('FileName,DecalId,ImageId\n')
 
     threads = []
     for l in split_files:
-        thread = threading.Thread(target=ThreadShit.start, args=(l,barrier,))
+        thread = threading.Thread(target=ThreadShit.start, args=(l,ROBLOSECURITY,outfile,))
         thread.start()
         threads.append(thread)
 
-    barrier.wait()
+    for thread in threads:
+        thread.join()
+
+    outfile.close()
 
