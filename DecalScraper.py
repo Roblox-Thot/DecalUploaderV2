d5ca62872fb0e46bf3ef9a6d3ffade5a9e2e5528
Roblox_Thot <67937010+Roblox-Thot@users.noreply.github.com>
2024-03-08 05:50:37 -0500

    modhual

--- DecalScraper.py
+++ DecalScraper.py
@@ -1,21 +1,20 @@
-from requests import session,exceptions
-sussion=session()
+from requests import get
 BASED = 'https://www.roblox.com/users/inventory/list-json?assetTypeId=13&cursor=&itemsPerPage=100&pageNumber={}&userId={}'
 
-def get_decals(userId, decals=None, pageNumber=1, lastLength=float('inf')):
+def get_decals(userId:int, decals:list = None, pageNumber:int = 1, lastLength:int = float('inf')):
     if decals is None:
         decals = []
 
     request_url = BASED.format(pageNumber, userId)
     try:
-        response = sussion.get(request_url)
+        response = get(request_url)
         response.raise_for_status()
         result2 = response.json()
 
         if result2:
-            for gamepass in result2['Data']['Items']:
-                if gamepass['Creator']['Id'] == userId and gamepass['Item']['AssetId'] not in decals:
-                    id = gamepass['Item']['AssetId']
+            for decal in result2['Data']['Items']:
+                if decal['Creator']['Id'] == userId and decal['Item']['AssetId'] not in decals:
+                    id = decal['Item']['AssetId']
                     decals.append(id)
 
             if len(response.text) != lastLength:
@@ -27,6 +26,7 @@ def get_decals(userId, decals=None, pageNumber=1, lastLength=float('inf')):
 
     return decals
 
-print("Note: inv must be public! (13+)\n\n")
-userGamepasses = get_decals(int(input("UserId: ")))
-print(userGamepasses)
+if '__main__' in __name__:
+    print('Note: inv must be public! (13+)\n\n')
+    userGamepasses = get_decals(int(input('UserId: ')))
+    print(userGamepasses)
