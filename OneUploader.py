464dbdebeb204c3ad6952210cb4658f5743917a5
Roblox-Thot <67937010+Roblox-Thot@users.noreply.github.com>
2024-05-15 17:24:10 -0400

    forgor

--- OneUploader.py
+++ OneUploader.py
@@ -119,7 +119,7 @@ if '__main__' in __name__:
                 
                 for x in range(0, width, square_size):
                     for y in range(0, height, square_size):
-                        static_color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+                        static_color = (random.randint(0, intensity), random.randint(0, intensity), random.randint(0, intensity))
                         static_image.paste(static_color, (x, y, min(x + square_size, width), min(y + square_size, height)))
 
                 data = Image.blend(rgba, static_image, 0.6)
